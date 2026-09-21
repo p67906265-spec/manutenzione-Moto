d1532f41908f77c3ad1f863ce4d63d557c1d1803
Il tuo nome <tua-email@esempio.com>
2026-09-21 21:32:35 +0200

    Versione 2.5 splash multilingua

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.paolofree.manutenzionemoto"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "2.4"
+        versionCode = 16
+        versionName = "2.5"
     }
 
     if (signingReady) {
